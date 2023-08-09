847aec3b8ff6a1ce6ac82aa2261c0a1141437177
Dawid Wojtaszek <dawidw22@gmail.com>
2023-08-09 12:16:43 +0200

    configuration

---
+++ Results summary component/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      lightRed: "hsl(0, 100%, 67%)",
+      orangeyYellow: "hsl(39, 100%, 56%)",
+      greenTeal: "hsl(166, 100%, 37%)",
+      cobaltBlue: "hsl(234, 85%, 45%)",
+      white: "hsl(0, 0%, 100%)",
+      paleBlue: "hsl(221, 100%, 96%)",
+      lightLavender: "hsl(241, 100%, 89%)",
+      darkGrayBlue: "hsl(224, 30%, 27%)",
+    },
+    fontFamily: {
+      disply: ["Hanken Grotesk", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
