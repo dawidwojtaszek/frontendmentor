de390c0a7b2f3e01afb18de54d5d2a4b85ff7364
Dawid Wojtaszek <dawidw22@gmail.com>
2023-08-10 12:45:17 +0200

    feat: add first styling

--- Results summary component/tailwind.config.js
+++ Results summary component/tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
       paleBlue: "hsl(221, 100%, 96%)",
       lightLavender: "hsl(241, 100%, 89%)",
       darkGrayBlue: "hsl(224, 30%, 27%)",
+      violetBlue: "hsla(256, 72%, 46%, 1)",
+      persianBlue: "hsla(241, 72%, 46%, 0)",
+      lightSlateBlue: "hsl(252, 100%, 67%)",
+      lightRoyalBlue: "hsl(241, 81%, 54%)",
     },
     fontFamily: {
       disply: ["Hanken Grotesk", "sans-serif"],
