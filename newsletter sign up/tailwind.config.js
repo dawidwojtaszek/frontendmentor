bf7e0a92e1ef2d86fe6a8d35fdd8344f891b3e11
Dawid Wojtaszek <dawidw22@gmail.com>
2023-10-25 12:47:02 +0200

    newsletter sign up: project config

---
+++ newsletter sign up/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      tomato: "hsl(4, 100%, 67%)",
+      darkSlateGrey: "hsl(234, 29%, 20%)",
+      charcoalGrey: "hsl(235, 18%, 26%)",
+      grey: "hsl(231, 7%, 60%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+    fontFamily: {
+      disply: ["Roboto", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
