4946842e3cbfce68b7262bbdd7658f0c1d1102cf
Dawid Wojtaszek <dawidw22@gmail.com>
2023-10-26 09:37:50 +0200

    finish desktop design

--- newsletter sign up/tailwind.config.js
+++ newsletter sign up/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     colors: {
       tomato: "hsl(4, 100%, 67%)",
+      gradient: "#ff537b",
       darkSlateGrey: "hsl(234, 29%, 20%)",
       charcoalGrey: "hsl(235, 18%, 26%)",
       grey: "hsl(231, 7%, 60%)",
