41a713618f7132c4162da1eddf12e4c8c957dfe0
Dawid Wojtaszek <dawidw22@gmail.com>
2024-01-04 12:27:16 +0100

    project config

--- todo-app-main/tailwind.config.js
+++ todo-app-main/tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      veryLightGray: "hsl(0, 0%, 98%)",
+      veryLightGrayishBlue: "hsl(236, 33%, 92%)",
+      lightGrayishBlue: "hsl(233, 11%, 84%)",
+      darkGrayishBlue: "hsl(236, 9%, 61%)",
+      veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+      veryDarkBlue: "hsl(235, 21%, 11%)",
+      veryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
+      lightGrayishBlueDark: "hsl(234, 39%, 85%)",
+      darkGrayishBlueDark: "hsl(234, 11%, 52%)",
+      veryDarkGrayishBlueDark: "hsl(233, 14%, 35%)",
+      veryDarkGrayishBlueDark2: "hsl(237, 14%, 26%)",
     },
+    extend: {},
   },
   plugins: [],
-}
+};
