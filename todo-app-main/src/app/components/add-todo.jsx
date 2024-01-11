97aee7b1e7a8f40b8c841f49ac85489cd9217f5c
Dawid Wojtaszek <dawidw22@gmail.com>
2024-01-11 13:03:20 +0100

    create add-todo comoponent

---
+++ todo-app-main/src/app/components/add-todo.jsx
@@ -0,0 +1,38 @@
+"use client";
+import { v4 as uuid } from "uuid";
+import { useState } from "react";
+import { useMyContext } from "../context/context";
+const AddTodo = () => {
+  const { globalState, updateGlobalState } = useMyContext();
+  const [inputState, setInputState] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = uuid();
+    updateGlobalState([
+      ...globalState,
+      { id: id, name: inputState, active: true },
+    ]);
+    setInputState("");
+  };
+  const handleChange = (e) => {
+    setInputState(e.target.value);
+  };
+  return (
+    <div className=" bg-white w-[540px] h-16 mx-auto rounded-md mb-6 px-6 py-5 flex items-center justify-start dark:bg-veryDarkDesaturatedBlue  ">
+      <div className=" flex-shrink-0 w-[24px] h-[24px] rounded-full bg-veryLightGrayishBlue flex items-center justify-center dark:bg-veryDarkGrayishBlue">
+        <div className="w-[22px] h-[22px] bg-white rounded-full dark:bg-veryDarkDesaturatedBlue"></div>
+      </div>
+      <form onSubmit={handleSubmit} className="w-full">
+        <input
+          type="text"
+          className="w-full ml-6 dark:bg-veryDarkDesaturatedBlue focus:outline-none dark:text-veryLightGrayishBlue"
+          placeholder="Create a new todo..."
+          onChange={handleChange}
+          value={inputState}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default AddTodo;
--- todo-app-main/src/app/components/todo-container.jsx
+++ todo-app-main/src/app/components/todo-container.jsx
@@ -1,32 +1,15 @@
 "use client";
-import { v4 as uuid } from "uuid";
 import Todo from "./todo";
+import AddTodo from "./add-todo";
 import { useMyContext } from "../context/context";
 import { useState, useEffect } from "react";
 
 const TodoContainer = () => {
-  const { globalState, upadateGlobalState } = useMyContext();
+  const { globalState, updateGlobalState } = useMyContext();
   const [currentFilter, setCurrentFilter] = useState("all");
   const [filteredTodos, setFilteredTodos] = useState([]);
 
   const itemsLeft = globalState.filter((e) => e.active === true);
-
-  // const filterList = (filterName) => {
-  //   switch (filterName) {
-  //     case "all":
-  //       tempList = globalState;
-  //       setCurrentList(tempList);
-  //       break;
-  //     case "active":
-  //       tempList = globalState.filter((e) => e.active === true);
-  //       setCurrentList(tempList);
-  //       break;
-  //     case "completed":
-  //       tempList = globalState.filter((e) => e.active === false);
-  //       setCurrentList(tempList);
-  //       break;
-  //   }
-  // };
 
   useEffect(() => {
     switch (currentFilter) {
@@ -45,9 +28,17 @@ const TodoContainer = () => {
     }
   }, [globalState, currentFilter]);
 
+  const handleClear = () => {
+    const clearedList = globalState.filter((e) => e.active === true);
+    updateGlobalState(clearedList);
+  };
+
   return (
     <div className="w-full absolute mt-[-140px] mx-auto ">
-      <div className=" bg-white w-[540px] h-16 mx-auto rounded-md mb-6 px-6 py-5 dark:bg-veryDarkDesaturatedBlue  "></div>
+      {/* <div className=" bg-white w-[540px] h-16 mx-auto rounded-md mb-6 px-6 py-5 dark:bg-veryDarkDesaturatedBlue  ">
+
+      </div> */}
+      <AddTodo />
       <div className="w-[540px] mx-auto overflow-hidden rounded-md bg-white   dark:bg-veryDarkDesaturatedBlue ">
         {filteredTodos.map((e) => (
           <Todo key={e.id} todo={e} />
@@ -57,7 +48,9 @@ const TodoContainer = () => {
           <div className=" ">{itemsLeft.length} items left</div>
           <div>
             <button
-              className="active-btn hover:text-veryDarkBlue dark:hover:text-veryLightGray"
+              className={`${
+                currentFilter === "all" ? "active-btn" : ""
+              } hover:text-veryDarkBlue dark:hover:text-veryLightGray`}
               onClick={() => {
                 setCurrentFilter("all");
               }}
@@ -65,7 +58,9 @@ const TodoContainer = () => {
               All
             </button>
             <button
-              className=" hover:text-veryDarkBlue dark:hover:text-veryLightGray mx-5"
+              className={`${
+                currentFilter === "active" ? "active-btn" : ""
+              } mx-5 hover:text-veryDarkBlue dark:hover:text-veryLightGray`}
               onClick={() => {
                 setCurrentFilter("active");
               }}
@@ -73,7 +68,9 @@ const TodoContainer = () => {
               Active
             </button>
             <button
-              className="hover:text-veryDarkBlue dark:hover:text-veryLightGray "
+              className={`${
+                currentFilter === "completed" ? "active-btn" : ""
+              } hover:text-veryDarkBlue dark:hover:text-veryLightGray`}
               onClick={() => {
                 setCurrentFilter("completed");
               }}
@@ -82,7 +79,10 @@ const TodoContainer = () => {
             </button>
           </div>
           <div>
-            <button className="hover:text-veryDarkBlue  dark:hover:text-veryLightGray">
+            <button
+              className="hover:text-veryDarkBlue  dark:hover:text-veryLightGray"
+              onClick={handleClear}
+            >
               Clear Completed
             </button>
           </div>
