ed89151f2b0571b1b293d345873796ec7c44f37e
Dawid Wojtaszek <dawidw22@gmail.com>
2024-01-15 11:09:10 +0100

    add reordering

--- todo-app-main/src/app/components/todo-container.jsx
+++ todo-app-main/src/app/components/todo-container.jsx
@@ -4,6 +4,7 @@ import AddTodo from "./add-todo";
 import { useMyContext } from "../context/context";
 import { useState, useEffect } from "react";
 import { Droppable } from "@hello-pangea/dnd";
+import { DragDropContext } from "@hello-pangea/dnd";
 
 const TodoContainer = () => {
   const { globalState, updateGlobalState } = useMyContext();
@@ -29,30 +30,83 @@ const TodoContainer = () => {
     }
   }, [globalState, currentFilter]);
 
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+    if (result.source.index === result.destination.index) return;
+    const arr = [...globalState];
+    const item = globalState[result.source.index];
+    arr.splice(result.source.index, 1);
+    arr.splice(result.destination.index, 0, item);
+    return updateGlobalState(arr);
+  };
+
   const handleClear = () => {
     const clearedList = globalState.filter((e) => e.active === true);
     updateGlobalState(clearedList);
   };
 
   return (
-    <div className="w-full absolute mt-[-140px] mx-auto ">
-      <AddTodo />
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <div className="w-full absolute mt-[-140px] mx-auto ">
+        <AddTodo />
 
-      <div className="w-[325px] mx-auto overflow-hidden rounded-md shadow-lg md:w-[540px]">
-        <Droppable droppableId="droppable1" type="group">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {filteredTodos.map((e, index) => (
-                <Todo key={e.id} todo={e} index={index} />
-              ))}
-              {provided.placeholder}
+        <div className="w-[325px] mx-auto overflow-hidden rounded-md shadow-lg md:w-[540px]">
+          <Droppable droppableId="droppable1" type="group">
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {filteredTodos.map((e, index) => (
+                  <Todo key={e.id} todo={e} index={index} />
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+
+          <div className="rounded-md text-xs h-[50px] px-6 py-4 flex justify-between items-center bg-white   dark:bg-veryDarkDesaturatedBlue text-darkGrayishBlue md:text-sm">
+            <div className=" ">{itemsLeft.length} items left</div>
+            <div className="hidden md:block">
+              <button
+                className={`${
+                  currentFilter === "all" ? "active-btn" : ""
+                } hover:text-veryDarkBlue dark:hover:text-veryLightGray`}
+                onClick={() => {
+                  setCurrentFilter("all");
+                }}
+              >
+                All
+              </button>
+              <button
+                className={`${
+                  currentFilter === "active" ? "active-btn" : ""
+                } mx-5 hover:text-veryDarkBlue dark:hover:text-veryLightGray`}
+                onClick={() => {
+                  setCurrentFilter("active");
+                }}
+              >
+                Active
+              </button>
+              <button
+                className={`${
+                  currentFilter === "completed" ? "active-btn" : ""
+                } hover:text-veryDarkBlue dark:hover:text-veryLightGray`}
+                onClick={() => {
+                  setCurrentFilter("completed");
+                }}
+              >
+                Completed
+              </button>
             </div>
-          )}
-        </Droppable>
-
-        <div className="rounded-md text-xs h-[50px] px-6 py-4 flex justify-between items-center bg-white   dark:bg-veryDarkDesaturatedBlue text-darkGrayishBlue md:text-sm">
-          <div className=" ">{itemsLeft.length} items left</div>
-          <div className="hidden md:block">
+            <div>
+              <button
+                className="hover:text-veryDarkBlue  dark:hover:text-veryLightGray"
+                onClick={handleClear}
+              >
+                Clear Completed
+              </button>
+            </div>
+          </div>
+          {/* Mobile */}
+          <div className=" rounded-md mt-4 bg-white text-xs h-12 w-full overflow-hidden flex justify-center items-center md:hidden dark:bg-veryDarkDesaturatedBlue dark:text-veryLightGrayishBlue">
             <button
               className={`${
                 currentFilter === "all" ? "active-btn" : ""
@@ -84,53 +138,12 @@ const TodoContainer = () => {
               Completed
             </button>
           </div>
-          <div>
-            <button
-              className="hover:text-veryDarkBlue  dark:hover:text-veryLightGray"
-              onClick={handleClear}
-            >
-              Clear Completed
-            </button>
-          </div>
         </div>
-        {/* Mobile */}
-        <div className=" rounded-md mt-4 bg-white text-xs h-12 w-full overflow-hidden flex justify-center items-center md:hidden dark:bg-veryDarkDesaturatedBlue dark:text-veryLightGrayishBlue">
-          <button
-            className={`${
-              currentFilter === "all" ? "active-btn" : ""
-            } hover:text-veryDarkBlue dark:hover:text-veryLightGray`}
-            onClick={() => {
-              setCurrentFilter("all");
-            }}
-          >
-            All
-          </button>
-          <button
-            className={`${
-              currentFilter === "active" ? "active-btn" : ""
-            } mx-5 hover:text-veryDarkBlue dark:hover:text-veryLightGray`}
-            onClick={() => {
-              setCurrentFilter("active");
-            }}
-          >
-            Active
-          </button>
-          <button
-            className={`${
-              currentFilter === "completed" ? "active-btn" : ""
-            } hover:text-veryDarkBlue dark:hover:text-veryLightGray`}
-            onClick={() => {
-              setCurrentFilter("completed");
-            }}
-          >
-            Completed
-          </button>
+        <div className="flex w-full justify-center mt-10 text-darkGrayishBlue md:mt-12">
+          Drag and drop to reorder list
         </div>
       </div>
-      <div className="flex w-full justify-center mt-10 text-darkGrayishBlue md:mt-12">
-        Drag and drop to reorder list
-      </div>
-    </div>
+    </DragDropContext>
   );
 };
 export default TodoContainer;
--- todo-app-main/src/app/page.js
+++ todo-app-main/src/app/page.js
@@ -2,16 +2,26 @@
 import Header from "./components/header";
 import TodoContainer from "./components/todo-container";
 import { DragDropContext } from "@hello-pangea/dnd";
+import { useMyContext } from "./context/context";
+
 export default function Home() {
-  const handleDragEnd = (result) => {
-    console.log("drag end");
-  };
+  // const { globalState, updateGlobalState } = useMyContext();
+
+  // const handleDragEnd = (result) => {
+  //   if (!result.destination) return;
+  //   if (result.source.index === result.destination.index) return;
+  //   const arr = [...globalState];
+  //   const item = globalState[result.source.index];
+  //   arr.splice(result.source.index, 1);
+  //   arr.splice(result.destination.index, 0, item);
+  //   return updateGlobalState(arr);
+  // };
   return (
     <main>
       <Header />
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <TodoContainer />
-      </DragDropContext>
+      {/* <DragDropContext onDragEnd={handleDragEnd}> */}
+      <TodoContainer />
+      {/* </DragDropContext> */}
     </main>
   );
 }
