3a4a33696d3e0b7dae737f09ca78c9943f171289
Dawid Wojtaszek <dawidw22@gmail.com>
2024-03-13 12:10:15 +0100

    create hero section

--- manage-landing-page/app/components/header.jsx
+++ manage-landing-page/app/components/header.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../public/images/logo.svg";
--- manage-landing-page/app/components/hero-section.jsx
+++ manage-landing-page/app/components/hero-section.jsx
@@ -1,3 +1,22 @@
-const HeroSection = () => <div className="h-[600px]">Hero</div>;
+import Image from "next/image";
+import HeroImage from "../../public/images/illustration-intro.svg";
+import Button from "./button";
+const HeroSection = () => (
+  <div className="flex flex-col-reverse w-full items-center md:flex-row md:justify-between md:min-h-[700px]">
+    <div className="max-w-[450px] flex flex-col items-center md:items-start">
+      <h1 className="  text-darkBlue font-bold text-5xl">
+        Bring everyone together to build better products.
+      </h1>
+      <p className="my-[40px] text-darkGrayishBlue md:max-w-[350px]">
+        Manage makes it simple for software teams to plan day-to-day tasks while
+        keeping the larger team goals in view.
+      </p>
+      <Button>Get Started</Button>
+    </div>
+    <div>
+      <Image src={HeroImage} alt="illustration" />
+    </div>
+  </div>
+);
 
 export default HeroSection;
