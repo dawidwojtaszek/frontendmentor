5d698e5d2934a90b6e56eebafabf930b68285f14
Dawid Wojtaszek <dawidw22@gmail.com>
2024-03-15 09:26:16 +0100

    fix header position in hero section

--- manage-landing-page/app/components/hero-section.jsx
+++ manage-landing-page/app/components/hero-section.jsx
@@ -4,7 +4,7 @@ import Button from "./button";
 const HeroSection = () => (
   <div className="flex flex-col-reverse w-full items-center md:flex-row md:justify-between md:min-h-[700px] mb-[60px]">
     <div className="md:max-w-[450px] flex flex-col items-center md:items-start bg-mobile-main-pattern bg-no-repeat bg-[length:448px_448px] bg-[top_-100px_right_-313px] px-6 md:px-0 md:bg-none ">
-      <h1 className="  text-darkBlue font-bold text-5xl">
+      <h1 className="  text-darkBlue font-bold text-5xl text-center md:text-left">
         Bring everyone together to build better products.
       </h1>
       <p className="my-[40px] text-darkGrayishBlue md:max-w-[350px] text-center md:text-left">
